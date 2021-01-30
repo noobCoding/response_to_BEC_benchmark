8483dcfb6b5c6067654648e5584eaff7dde8aa26
hainct <justosue@gmail.com>
2021-01-30 21:19:47 +0900

    Add files via upload

--- data/mouse_cell_atlas_data.R
+++ data/mouse_cell_atlas_data.R
@@ -2,24 +2,25 @@ library(scater)
 rm(list=ls())
 
 base_name='mouse_cell_atlas'
-counts <- read.table("../ds2/filtered_total_batch1_seqwell_batch2_10x.txt", head=T, sep='\t')
-metadata <- read.table("../ds2/filtered_total_sample_ext_organ_celltype_batch.txt", head=T, sep='\t')
+counts <- read.table("ds2/filtered_total_batch1_seqwell_batch2_10x.txt", head=T, sep='\t')
+metadata <- read.table("ds2/filtered_total_sample_ext_organ_celltype_batch.txt", head=T, sep='\t')
 rownames(metadata) <- gsub("-", "\\.", rownames(metadata))
 
 K_intervals = c(2, 3, 4, 6, 7, 8)
+cutoff = 0.95
+a = b = 5
+pp = 1
 
 for (K in K_intervals)
 {
   batch <- metadata$batch
   tcell = which(metadata$orig.ident=='T-cell')
-  batch1 = counts[, which(batch[tcell]==1)]
-  batch2 = counts[, which(batch[tcell]==2)]
-  batch <- batch[tcell]
+  tcount= counts[, tcell]
+  tbatch= batch[tcell]
   
-  batch <- c(colnames(batch1), colnames(batch2))
-  batch[] = '2'                           
-  batch[1:length(colnames(batch1))] = '1'   
-
+  batch1 = tcount[, which(tbatch==1)]
+  batch2 = tcount[, which(tbatch==2)]
+  
   # Shuffling replicates    
   ori_cell_order <- colnames(batch1)
   sp_cells <- colnames(batch1)
@@ -33,15 +34,12 @@ for (K in K_intervals)
   batch2 <- batch2[, sp_cells]
   colnames(batch2) <- ori_cell_order
   
-  group <- batch
+  group <- tbatch
   for (i in unique(group)){
     gid = which(group==i)
     group[gid[1:(length(gid)%/%10*K)]] = paste0(i, "_A")
     group[gid[(length(gid)%/%10*K + 1):length(gid) ]] = paste0(i, "_B")
   }
-  
-  cutoff = 0.95
-  a = b = 5
   
   rowsum = rowSums(as.matrix(batch1)==0)/length(colnames(batch1))
   rowsum2 = rowSums(as.matrix(batch2)==0)/length(colnames(batch2))
@@ -54,18 +52,20 @@ for (K in K_intervals)
   
   batch1 = batch1[common_genes, ]   
   batch2 = batch2[common_genes, ]   
-  colnames(batch1)<-(group[which(batch==1)])  
-  colnames(batch2)<-(group[which(batch==2)])
+  colnames(batch1)<-group[which(tbatch==1)]  
+  colnames(batch2)<-group[which(tbatch==2)]
   
-  N = length(common_genes) %/% 5  #
+  N = round(length(common_genes) *pp /50)
   if (N%%2==1) N = N + 1
-  
+  print (N)
   DEG_groundthruth = sample(common_genes, N, replace=FALSE)
   second_N_genes  = sample(DEG_groundthruth, N%/%2, replace = FALSE)
   first_N_genes= DEG_groundthruth[!DEG_groundthruth %in% second_N_genes]
   
   #Down sampling
-  ############################# batch 1
+  batch1_ori = batch1
+  batch2_ori = batch2
+  
   tmp <- batch1[first_N_genes, which(colnames(batch1)==unique(colnames(batch1))[1])]
   for (g in rownames(tmp)){
     for (c in colnames(tmp)){
@@ -106,6 +106,10 @@ for (K in K_intervals)
   ##################################################
   newmat <- cbind.DataFrame(batch1, batch2)
   
+  if (anyNA(newmat)) {
+    cat(paste0(K, "-NAs exist!"))
+    exit
+  }
   # colsum = colSums(as.matrix(newmat) == 0)
   # simsparsity = sum(colsum)/(length(rownames(newmat))*length(colnames(newmat)))
   # print(simsparsity)
@@ -118,7 +122,7 @@ for (K in K_intervals)
   
   newlabel<-c('label',newgroup)
   
-  newbatch<-batch
+  newbatch<-tbatch
   geneinfo <- common_genes
   cellinfo <- as.data.frame(newgroup)
   cellinfo$batch <- factor(newbatch)
@@ -128,7 +132,7 @@ for (K in K_intervals)
   down_genes <- first_N_genes
   de_genes <- DEG_groundthruth
   
-  ds = paste0("mca_dgsp2095_b155_13_", K)
+  ds = paste0("mca_dgsp95_b155_",pp,"_", K)
   dir.create(ds)
   write.table(newmat, file = paste0(ds,"/counts.txt"), sep = "\t", row.names = TRUE, col.names = TRUE)
   write.table(geneinfo, file = paste0(ds,"/geneinfo.txt"), sep = "\t", row.names = TRUE, col.names = TRUE)
